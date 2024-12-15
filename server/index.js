c034198b9f76f620f3242569b4b48e2fd05fb5b3
karthik Sudhan <karthiksudhan.sg2021@vitstudent.ac.in>
2024-12-15 11:02:41 +0530

    Done Login register and auth

--- server/index.js
+++ server/index.js
@@ -1,20 +1,27 @@
 const express=require("express")
 const cors = require("cors")
 const mongoose=require("mongoose")
+const userRoutes=require("./routes/userRoutes");
 
 const app=express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", userRoutes);
+
 
 const server=app.listen(process.env.PORT,()=>{
     console.log(`server Started on port ${process.env.PORT}` );
 })
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log("DB connection sucessfull");
-}).catch((err)=>{
-    console.log(err.message);
-});
+mongoose.connect("mongodb://localhost:27017/chat-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log(err));
+
+
+
+
